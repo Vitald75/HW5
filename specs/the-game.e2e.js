89e3133d17d4285b4e147e2accb83c0e92a35e53
Vitali Dolgoshei <Vitald75@gmail.com>
2021-12-11 23:46:55 +0300

    HW5

--- specs/the-game.e2e.js
+++ specs/the-game.e2e.js
@@ -1,13 +1,27 @@
+
 describe('The game', function () {
     it('should get more than 100 points ', async function () {
-        await browser.url('http://localhost:63342/course-sut/arkanoid.html');
+        await browser.url('https://viktor-silakov.github.io/course-sut/arkanoid.html');
         await browser.pause(1000);
         await $('button=PLAY').click();
         await browser.waitUntil(async () => {
-            // player logic
-            const points = parseInt(await $('#points').getText(), 10);
+        // player logic
+       
+        let ballLocationX = await $('#ball').getLocation('x');
+        let padLocationX = (await $('#pad').getLocation('x')) + 60;
+        if (ballLocationX > (padLocationX)) {
+            await browser.keys('D');
+            await browser.keys('D');
+           } else if (ballLocationX < padLocationX) {
+            await browser.keys('A');
+            await browser.keys('A');
+        }
+
+        const points = parseInt(await $('#points').getText(), 10);
             if (points > 100) return true
-            console.log({ points })
+            console.log({ points });
+            console.log(mutationObserver.takeRecords());
+
         }, { timeout: 600000, interval: 10 })
     });
 })
